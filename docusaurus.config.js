e75534c76b1b84bd80f4abbba986d5fd6c92656d
VolcanicArts <VolcanicArtsBP@Gmail.com>
2024-07-11 19:53:58 +0100

    Update base Url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://VolcanicArts.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/VRCOSC-Docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
